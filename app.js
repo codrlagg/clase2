897000f00fd2dc6ecfcd9cec98dfc1024a32a3d7
luis garcia <luigy-antoni@outlook.com>
2021-11-06 01:12:54 -0600

    Divisor con sentencia or

--- app.js
+++ app.js
@@ -138,6 +138,29 @@ else if(menum == 3){
         alert("Valor inválido");
     }
 }
+// submenu para division
+else if(menum == 4){
+    console.log("Operación seleccionada: division");
+    console.log("Variables a dividir: dos");
+    // solicitar datos variable 1
+    let var1 = prompt("Ingresa el primer número a dividir");
+    let var1num = parseInt(var1);
+    console.log("var1: "+var1num);
+    // solicitar datos variable 2
+    let var2 = prompt("Ingresa el valor del divisor");
+    if((var2 =="")||(var2 == 0)){
+        console.log("Ingresar un número válido");
+        alert("Ingresa un número válido");
+    }
+    else{
+        let var2num = parseInt(var2);
+        console.log("var2: "+var2num);
+        // dividir valores
+        let resultado = var1num * var2num;
+        console.log("resultado: "+resultado);
+        alert("La division es " + resultado);
+        }
+}
 else {
     console.log("No furula");
 }
